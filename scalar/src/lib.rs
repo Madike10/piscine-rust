78ea5ba2da42feb105a91332115b5dbf6438632c
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-27 12:12:59 +0000

    scalar2

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,15 +1,15 @@
 pub fn sum(a : u8, b : u8) -> u8{
-    a + b;
+   return a + b;
 }
 
 pub fn diff(a : i16, b : i16) -> i16{
-    a - b;
+   return a - b;
 }
 
 pub fn quo(a : i8, b : i8) -> i8{
-    a * b;
+    return a * b;
 }
 
 pub fn rem(a : f32, b : f32) -> f32{
-    a % b;
+   return a % b;
 }
