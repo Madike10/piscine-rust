46b6f399da407dd7618ed9a98d08c586922642ba
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-27 19:11:11 +0000

    tuples_refs 2

--- tuples_refs/src/lib.rs
+++ tuples_refs/src/lib.rs
@@ -2,14 +2,14 @@
 pub struct Student (pub u32, pub String, pub String);
 
 pub fn id(student: &Student) -> u32 {
-    Student.0
+    student.0
 
 }
 
 pub fn first_name(student: &Student) -> String {
-    Student.1.to_string()
+    student.1.to_string()
 }
 
 pub fn last_name(student: &Student) -> String {
-    Student.2.to_string()
+    student.2.to_string()
 }
