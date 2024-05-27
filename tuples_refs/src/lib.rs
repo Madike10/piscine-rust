9209df06119de6568a3370421c60b81f2f4db2f3
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-27 19:00:52 +0000

    tuples_refs

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,15 @@
+
+struct  Student (u32, String, String)
+
+pub fn id(student: &Student) -> u32 {
+    Student.0
+
+}
+
+pub fn first_name(student: &Student) -> String {
+    Student.1
+}
+
+pub fn last_name(student: &Student) -> String {
+    Student.2
+}
