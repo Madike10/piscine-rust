2f7c77cacbd4f9418dc56d50fe3ea96882acaaa9
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-28 13:43:19 +0000

    doubtful

---
+++ doubtful/src/lib.rs
@@ -0,0 +1,14 @@
+pub fn doubtful(s : &mut String){
+    s.push('?');
+}
+ 
+fn main() {
+	let mut s = String::from("Hello");
+
+	println!("Before changing the string: {}", s);
+
+	doubtful(&mut s);
+
+	println!("After changing the string: {}", s);
+}
+
