428301cf6fff8a888475594e48e608ef5ef0361a
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-28 17:44:54 +0000

    name_initials

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,22 +1,24 @@
 pub fn initials(names: Vec<&str>) -> Vec<String> {
-    let v : Vec(String) = Vec::new();
-
+    let mut v : Vec<String> = Vec::new();
     for name in names{
-        let namesplit = name.split(" ")
-        for n in namesplit{
-            
+       let mut index = 0;
+       let mut temp = String::new();
+       let namesplit = name.split(" ");
+       for n in namesplit {
+        temp.push(n.chars().nth(0).unwrap());
+        temp.push('.');
+        if index == 0 {
+            temp.push(' ');
+            index +=1;
         }
-
+       }
+       v.push(temp);
     }
+    return v;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn main() {
+    let names = vec!["Harry Potter", "Someone Else", "J. L.", "Barack Obama"];
+    println!("{:?}", initials(names));
 }
+
