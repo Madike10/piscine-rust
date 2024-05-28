2d95d6116f46cde0b275b70e35c918b9acd8623c
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-28 16:01:44 +0000

    name_initials

---
+++ name_initials/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn initials(names: Vec<&str>) -> Vec<String> {
+    let v : Vec(String) = Vec::new();
+
+    for name in names{
+        let namesplit = name.split(" ")
+        for n in namesplit{
+            
+        }
+
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
