90f68b105987e7d194398053ccfd8431bd0609cc
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-30 19:07:22 +0000

    bigger

---
+++ bigger/src/lib.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+pub fn bigger(h: HashMap<&str, i32>) -> i32 {
+    let mut biggest = 0;
+    for (_, value) in h {
+        if value > biggest {
+            biggest = value;
+        }
+    }
+    return biggest;
+}
+
+
+fn main() {
+
+    let mut hash = HashMap::new();
+    hash.insert("Daniel", 122);
+    hash.insert("Ashley", 333);
+    hash.insert("Katie", 334);
+    hash.insert("Robert", 14);
+
+    println!("The biggest of the elements in the HashMap is {}", bigger(hash));
+}
+
+// And its output
+
+// $ cargo run
+// The biggest of the elements in the HashMap is 334
+// $
