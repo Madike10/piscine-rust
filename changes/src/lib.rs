209d2cc4a20bd9f9e474292b64bee915f2f55a03
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-30 12:27:04 +0000

    change

---
+++ changes/src/lib.rs
@@ -0,0 +1,35 @@
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Light {
+	pub alias: String,
+	pub brightness: u8,
+}
+
+impl Light {
+	pub fn new(alias: &str) -> Self {
+        Light {
+            alias: alias.to_string(),
+            brightness: 0,
+        }
+	}
+}
+
+pub fn change_brightness(lights: &mut Vec<Light>, alias: &str, value: u8) {
+    for light in lights {
+        if light.alias == alias.to_string() {
+            light.brightness = value;
+        }
+    }
+}
+
+fn main() {
+	// bedroom
+	let mut lights = vec![
+		Light::new("living_room"),
+		Light::new("bedroom"),
+		Light::new("rest_room"),
+	];
+	println!("brightness = {}", lights[0].brightness);
+	change_brightness(&mut lights, "living_room", 200);
+	println!("new brightness = {}", lights[0].brightness);
+}
+
