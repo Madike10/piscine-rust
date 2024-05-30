48433af51aeec9d59833afd0c8a77ca4f073fcde
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-30 13:33:38 +0000

    circle

---
+++ circle/src/lib.rs
@@ -0,0 +1,72 @@
+#[derive(Debug)]
+pub struct Circle {
+	pub center  : Point,
+	pub radius  : f64,
+}
+
+impl Circle {
+    pub fn new(center: Point, radius: f64) -> Self {
+        Circle {
+            center: center,
+            radius: radius,
+        }
+    }
+    pub fn diameter(&self) -> f64 {
+         self.radius * 2.0
+    }
+    pub fn area(&self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
+    }
+    pub fn intersect(&self, c2 : &Self) -> bool{
+        self.center.distance(&c2.center) < self.radius + c2.radius
+    }
+
+}
+#[derive(Debug)]
+
+pub struct Point {
+   pub x : f64,
+   pub y : f64,
+}
+
+impl Point {
+    pub fn new (x : f64, y : f64) -> Self {
+        Point {
+            x : x,
+            y : y,
+        }
+    }
+    pub fn distance (&self, p : &Self) -> f64{
+        let dx = self.x - p.x ;
+        let dy = self.y - p.y ;
+        (dx * dx + dy * dy).sqrt()
+        
+    }
+}
+
+
+fn main() {
+    let center = Point { x: 500.0, y: 500.0 }; // Assurez-vous que Point est défini quelque part dans votre code
+    let circle = Circle::new(center, 150.0);
+	let circle1 = Circle {
+		center: Point { x: 80.0, y: 115.0 },
+		radius: 30.0,
+	};
+	let point_a = Point { x: 1.0, y: 1.0 };
+	let point_b = Point { x: 0.0, y: 0.0 };
+	println!("circle = {:?} area = {}", circle, circle.area());
+	println!("circle = {:?} diameter = {}", circle, circle.diameter());
+	println!("circle1 = {:?} diameter = {}", circle1, circle1.diameter());
+	println!(
+		"circle and circle1 intersect = {}",
+		circle.intersect(&circle1)
+	);
+
+	println!(
+		"distance between {:?} and {:?} is {}",
+		point_a,
+		point_b,
+		point_a.distance(&point_b)
+	);
+
+}
