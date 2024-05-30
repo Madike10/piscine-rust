b5da367bda11cb46043a4e8789b8103a15702bd7
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-30 15:42:48 +0000

    arrays

---
+++ arrays/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn sum(a : &[i32]) -> i32 {
+    // let mut result = 0;
+    // for num in &a {
+    //     result += num;
+    // }
+    // result
+    a.iter().sum()
+}
+
+pub fn thirtytwo_tens() -> [i32; 32] {
+    [10;32]
+}
