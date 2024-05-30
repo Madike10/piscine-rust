3877b25604f19c4125c42b2e0f26938988a279fe
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-30 18:36:23 +0000

    edit_distamce

---
+++ edit_distance/src/lib.rs
@@ -0,0 +1,44 @@
+pub fn edit_distance(source: &str, target: &str) -> usize {
+    let source = source.chars().collect::<Vec<char>>();
+    let target = target.chars().collect::<Vec<char>>();
+    let mut dp = vec![vec![0; target.len() + 1]; source.len() + 1];
+
+    for i in 0..source.len() + 1 {
+        dp[i][0] = i;
+    }
+
+    for j in 0..target.len() + 1 {
+        dp[0][j] = j;
+    }
+    for i in 1..source.len() + 1 {
+        for j in 1..target.len() + 1 {
+            if source[i - 1] == target[j - 1] {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = 1 + dp[i - 1][j - 1].min(dp[i - 1][j].min(dp[i][j - 1]));
+            }
+        }
+    }
+
+    // Return the calculated edit distance
+    dp[source.len()][target.len()]
+}
+
+
+fn main() {
+	let source = "alignment";
+	let target = "assignment";
+	println!(
+		"It's necessary to make {} change(s) to {}, to get {}",
+		edit_distance(source, target),
+		source,
+		target
+	);
+}
+
+// And its output:
+
+
+// $ cargo run
+// It's necessary to make 2 change(s) to alignment, to get assignment
+// $
