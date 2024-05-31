36e698a89fef451fd5a513d68c3934192ab8856e
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-31 19:04:57 +0000

    is_anagram 2

--- is_anagram/src/lib.rs
+++ is_anagram/src/lib.rs
@@ -1,10 +1,10 @@
 pub fn is_anagram(s1: &str, s2: &str) -> bool {
     let mut s1 = s1.to_lowercase();
     let mut s2 = s2.to_lowercase();
-    s1.sort();
-    s2.sort();
+    
+    // Convert the Strings to slices of characters and sort them
+    s1.sort_unstable();
+    s2.sort_unstable();
+    
     s1 == s2
-
 }
-
- 
