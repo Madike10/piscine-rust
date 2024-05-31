9e27c59f1f390c4e4f3fd058f39da453b5c87d60
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-05-31 18:21:53 +0000

    hashing 6

--- hashing/src/lib.rs
+++ hashing/src/lib.rs
@@ -1,40 +1,31 @@
 use std::collections::HashMap;
-pub fn mean(list: &Vec<i32>) -> f64 {
-    let mut sum = 0;
-    for num in list {
-        sum += num;
-    }
+
+pub fn mean(list: &[i32]) -> f64 {
+    let sum: i32 = list.iter().sum();
     sum as f64 / list.len() as f64
 }
 
-pub fn median(list: &Vec<i32>) -> i32 {
-    let mut list = list.clone();
-    list.sort();
-    if list.len() % 2 == 0 {
-        (list[list.len() / 2] + list[list.len() / 2 - 1]) / 2
+pub fn median(list: &mut [i32]) -> f64 {
+    list.sort_unstable();
+    let len = list.len();
+    if len % 2 == 0 {
+        (list[len / 2 - 1] + list[len / 2]) as f64 / 2.0
     } else {
-        list[list.len() / 2]
+        list[len / 2] as f64
     }
 }
 
-pub fn mode(list: &Vec<i32>) -> i32 {
+pub fn mode(list: &[i32]) -> i32 {
     let mut map = HashMap::new();
-    for num in list {
+    for &num in list {
         *map.entry(num).or_insert(0) += 1;
     }
-    let mut biggest = 0;
-    for (_, value) in &map {
-        if *value > biggest {
-            biggest = *value;
-        }
-    }
-    for (key, value) in map {
-        if value == biggest {
-            return *key;
-        }
-    }
-    return 0;
+    map.into_iter()
+        .max_by_key(|&(_, count)| count)
+        .map(|(num, _)| num)
+        .unwrap_or(0)
 }
+
 
 
 
