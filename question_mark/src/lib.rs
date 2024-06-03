3c1b6c6dcba2fe1f4d188a3fa92e35581ca5c5d4
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-03 17:22:10 +0000

    question_mark

--- question_mark/src/lib.rs
+++ question_mark/src/lib.rs
@@ -23,20 +23,20 @@ impl One {
 
 
 
-fn main() {
-    let a = One {
-        first_layer : Some(Two {
-            second_layer: Some(Three {
-                third_layer: Some(Four {
-                    fourth_layer: Some(1000)
-                })
-            })
-        })
-    };
+// fn main() {
+//     let a = One {
+//         first_layer : Some(Two {
+//             second_layer: Some(Three {
+//                 third_layer: Some(Four {
+//                     fourth_layer: Some(1000)
+//                 })
+//             })
+//         })
+//     };
 
-    // output: 1000
-    println!("{:?}", match a.get_fourth_layer() {
-        Some(e) => e,
-        None => 0
-    })
-}
+//     // output: 1000
+//     println!("{:?}", match a.get_fourth_layer() {
+//         Some(e) => e,
+//         None => 0
+//     })
+// }
