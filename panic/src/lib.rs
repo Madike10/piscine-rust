a1cd0bbb65f3f7cd5cd45d457040bff84179f459
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-03 13:57:53 +0000

    panic 2

--- panic/src/lib.rs
+++ panic/src/lib.rs
@@ -1,3 +1,21 @@
+use std::fs::File;
+use std::fs;
+// use panic::*;
+
 pub fn open_file(s: &str) -> File {
     File::open(s).unwrap()
+}
+
+
+fn main() {
+    let filename = "created.txt";
+    File::create(filename).unwrap();
+
+    let a = open_file(filename);
+    println!("{:?}", a);
+    
+    fs::remove_file(filename).unwrap();
+
+    //It must panic
+    let b = open_file(filename);
 }
