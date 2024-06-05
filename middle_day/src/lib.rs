0c6577f18f6a2349e64fb77c28615c70be93f1ab
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-05 13:36:58 +0000

    middle_day 1.0

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,17 +1,39 @@
 extern crate chrono;
-pub use chrono::{Datelike, NaiveDate, Weekday as wd};
-// Use the chrono crate to create a function named middle_day. It accepts a year, and returns the weekday of the middle day of that year, wrapped in an Option. chrono::Weekday has to be referred to as wd.
 
-// Years with an even number of days do not have a middle day, and should return None.
-#[warn(deprecated)]
-pub fn middle_day(year : i32) -> Option<wd>{
-    let mut date = NaiveDate::from_ymd_opt(year, 1, 1)?;
-    if date.weekday().number_from_monday() % 2 == 0 {
-        return None;
+pub use chrono::{Datelike, NaiveDate, Weekday as wd};
+
+pub fn middle_day(year: i32) -> Option<wd> {
+    /*
+        NaiveDate::from_ymd_opt(year, 12, 31) : Tente de créer une date pour
+        le 31 décembre de l'année spécifiée. La méthode from_ymd_opt retourne
+        une Option<NaiveDate>, qui est Some si la date est valide et None sinon
+        Le point d'interrogation ? déstructure l'Option, retournant None si la
+        date est invalide et continuant l'exécution si elle est valide.
+    */
+    // Cree une date pour le 31 12 de l'annee year
+    let lasday = NaiveDate::from_ymd_opt(year, 12, 31)?;
+    /*
+        lastday.ordinal() : Appelle la méthode ordinal sur last_day pour obtenir
+        le numéro ordinal du jour dans l'année (par exemple, 31 décembre est le 365e
+        jour de l'année pour une année non bissextile).
+    */
+    let day_in_year = lasday.ordinal();
+
+    /*
+        if days_in_year % 2 != 0 : Vérifie si le nombre de jours dans l'année est impair.
+        Si le nombre de jours est impair :
+        NaiveDate::from_ymd_opt(year, 1, 1)? : Crée une date pour le 1er janvier de l'année spécifiée.
+        checked_add_signed(chrono::Duration::days(days_in_year as i64 / 2)) : Ajoute la moitié du nombre
+        total de jours de l'année (troncature entière) au 1er janvier pour trouver le jour central.
+        .map(|d| d.weekday()) : Si l'addition réussit, transforme la date résultante en son jour de la
+        semaine (Weekday).
+        Si le nombre de jours est pair, la fonction retourne None.
+    */
+    if day_in_year % 2 != 0 {
+        NaiveDate::from_ymd_opt(year, 1, 1)?
+            .checked_add_signed(chrono::Duration::days(day_in_year as i64 / 2))
+            .map(|d| d.weekday())
+    } else {
+        None
     }
-    date = date.succ_opt()?;
-    while date.weekday().number_from_monday() % 2!= 0 {
-        date = date.succ_opt()?;
-    }
-    Some(date.weekday())
 }
