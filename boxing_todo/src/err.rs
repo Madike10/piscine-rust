ef61bc2c2371b9686f5c7a7929ff409efef3a698
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-05 02:07:43 +0000

    boxing_todo 1.0

--- boxing_todo/src/err.rs
+++ boxing_todo/src/err.rs
@@ -11,7 +11,7 @@ pub enum ParseErr {
 // required by error trait
 impl Display for ParseErr {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Fail to parses todo",)
+        write!(f, "Fail to parse todo",)
     }
 }
 
