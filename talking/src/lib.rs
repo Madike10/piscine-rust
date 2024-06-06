fa2f40f3584ec4c5c6b4fc30514296ae62fc1137
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-06 17:12:13 +0000

    talking 1.0

--- talking/src/lib.rs
+++ talking/src/lib.rs
@@ -1,26 +1,15 @@
-pub fn talking(text: &str) -> &str {
-    if (text.chars().all(check_all_upper)) && (text.chars().last() == Some('!')){
-        return &("There is no need to yell, calm down!");
-    }else if (!text.chars().all(check_all_upper)) && (text.chars().last() == Some('!')){
-        return &("Sure.");
-    }else if (text.chars().all(check_all_upper)) && (text.chars().last() == Some('?')){
-        return &("Quiet, I am thinking!");
-
-    }else if text == ""{
-        return &("Just say something!");
-    }else{
-        return &("Interesting");
+pub fn talking(input: &str) -> &str {
+    if input.trim().is_empty() {
+        "Just say something!"
+    } else if input == input.to_uppercase() && input.chars().any(|c| c.is_alphabetic()) {
+        if input.ends_with('?') {
+            "Quiet, I am thinking!"
+        } else {
+            "There is no need to yell, calm down!"
+        }
+    } else if input.ends_with('?') {
+        "Sure."
+    } else {
+        "Interesting"
     }
-}
-
-fn check_all_upper(c : char) -> bool{
-    c.is_uppercase() || !c.is_alphabetic()
-}
-
-fn main() {
-    println!("{:?}", talking("JUST DO IT!"));
-    println!("{:?}", talking("Hello how are you?"));
-    println!("{:?}", talking("WHAT'S GOING ON?"));
-    println!("{:?}", talking("something"));
-    println!("{:?}", talking(""));
 }
