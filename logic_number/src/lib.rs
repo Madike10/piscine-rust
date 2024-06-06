0ce9afb0066cd83e4fd884f5ffe24376156f73ba
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-06 17:37:26 +0000

    logic_number 1.0

--- logic_number/src/lib.rs
+++ logic_number/src/lib.rs
@@ -1,16 +1,13 @@
 pub fn number_logic(num: u32) -> bool {
     let mut sum = 0;
     let num_of_digits = num.to_string().len() as u32;
-
-    for digit in num.to_string().chars().map(|c| c.to_digit(10).unwrap()) {
+    for digit in num.to_string().chars().map(|c| c.to_digit(10).unwrap()){
         sum += digit.pow(num_of_digits);
     }
-
-    if sum == num {
+    if num == sum{
         return true;
-    } else {
-        return false;
     }
+    false
 }
 fn main() {
     let array = [9, 10, 153, 154];
