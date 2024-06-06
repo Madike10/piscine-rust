abb40293c85d319fdf27bdc9612331376ab84d06
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-06 15:07:22 +0000

    pangram 1.0

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,6 +1,7 @@
+use std::collections::HashSet;
 pub fn is_pangram(s: &str) -> bool {
     let mut s = s.to_lowercase();
-    let mut set = std::collections::HashSet::new();
+    let mut set = HashSet::new();
     for c in s.chars() {
         if c.is_ascii_alphabetic() {
             set.insert(c);
