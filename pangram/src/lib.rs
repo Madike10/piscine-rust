4d3286475a1c957675ed2bedb59696b2a9bda3a4
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-06 14:44:47 +0000

    pangram

---
+++ pangram/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn is_pangram(s: &str) -> bool {
+    let mut s = s.to_lowercase();
+    let mut set = std::collections::HashSet::new();
+    for c in s.chars() {
+        if c.is_ascii_alphabetic() {
+            set.insert(c);
+        }
+    }
+    set.len() == 26
+}
+
+fn main() {
+    println!(
+        "{}",
+        is_pangram("the quick brown fox jumps over the lazy dog!")
+    );
+    println!("{}", is_pangram("this is not a pangram!"));
+}
