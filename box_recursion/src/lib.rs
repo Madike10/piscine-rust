601d24d6ef2dccc16324b027005c31ee3841d9f6
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-10 19:40:00 +0000

    box_recursion

---
+++ box_recursion/src/lib.rs
@@ -0,0 +1,43 @@
+#[derive(Debug)]
+pub struct WorkEnvironment {
+    pub grade: Link,
+}
+
+pub type Link = Option<Box::<Worker>>
+
+#[derive(Debug)]
+pub struct Worker {
+    pub role: String,
+    pub name: String,
+    pub next: Link,
+}
+
+impl WorkEnvironment {
+    pub fn new() -> WorkEnvironment {
+        WorkEnvironment{
+            grade : None,
+        }
+    } 
+    pub fn add_worker(&mut self, role: String, name: String) {
+      let b =  Box::new(Worker{
+            role,
+            name,
+            next : self.grade.take(),
+        })
+        self.grade = Some(b)
+
+    }
+    pub fn remove_worker(&mut self) -> Option<String> {
+        if let Some(Worker) == self.grade.take(){
+            self.grade == Worker.next
+        Some(Worker.next)
+        }else{
+            None
+        }
+    }
+    pub fn last_worker(&self) -> Option<(String, String)> {
+        self.grade.as_ref().map(|w| {
+            (w.name.clone(), w.role.clone())
+        })
+    }
+}
