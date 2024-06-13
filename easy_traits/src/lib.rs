d33e692c04028cae2c4513c864398c7d7215ae6a
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-13 14:25:19 +0000

    easy_traits 1.3

--- easy_traits/src/lib.rs
+++ easy_traits/src/lib.rs
@@ -21,7 +21,7 @@ impl AppendStr for StringValue {
         self.clone()
     }
     fn remove_punctuation_marks(&mut self) -> Self{
-        self.value.retain(|c| c.is_ascii_alphanumeric() || c == ' ');
+        self.value.retain(|c| !c.is_ascii_punctuation());
         self.clone()
     }
 
@@ -29,16 +29,16 @@ impl AppendStr for StringValue {
 
 
 
-// fn main() {
-//     let mut str_aux = StringValue {
-//         value: String::from("hello"),
-//     };
-//     println!("Before append: {}", str_aux.value);
+fn main() {
+    let mut str_aux = StringValue {
+        value: String::from("hello"),
+    };
 
-//     str_aux.append_number(2.0);
-//     println!("After append: {}", str_aux.value);
+    println!("Before append: {}", str_aux.value);
 
-//     str_aux.remove_punctuation_marks();
-//     println!("After removing punctuation: {}", str_aux.value);
+    str_aux.append_str(String::from(" there!"));
+    println!("After append: {}", str_aux.value);
 
-// }
+    str_aux.remove_punctuation_marks();
+    println!("After removing punctuation: {}", str_aux.value);
+}
