d5331057bdae9a28ebaac7daf3037a46d63b09a1
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-19 14:12:28 +0000

    border_cross

---
+++ border_cross/src/lib.rs
@@ -0,0 +1,64 @@
+#[allow(dead_code)]
+pub struct Car<'a> {
+	pub plate_nbr: &'a str,
+	pub model: &'a str,
+	pub horse_power: u32,
+	pub year: u32,
+}
+
+#[allow(dead_code)]
+pub struct Truck<'a> {
+	pub plate_nbr: &'a str,
+	pub model: &'a str,
+	pub horse_power: u32,
+	pub year: u32,
+	pub load_tons: u32,
+}
+
+pub trait Vehicle {
+	fn model(&self) -> &str;
+	fn year(&self) -> u32;
+}
+
+impl Vehicle for Truck<'_> {
+    fn model(&self) -> &str {
+        self.model
+    }
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+impl Vehicle for Car<'_> {
+    fn model(&self) -> &str {
+        self.model
+    }
+    fn year(&self) -> u32 {
+        self.year
+    }
+}
+
+fn all_models(list: Vec<&dyn Vehicle>) -> Vec<&str> {
+    list.iter().map(|v| v.model()).collect()
+}
+
+fn main() {
+    let vehicles: Vec<Box<dyn Vehicle>> = vec![
+        Box::new(Car {
+            plate_nbr: "A3D5C7",
+            model: "Model 3",
+            horse_power: 325,
+            year: 2010,
+        }),
+        Box::new(Truck {
+            plate_nbr: "V3D5CT",
+            model: "Ranger",
+            horse_power: 325,
+            year: 2010,
+            load_tons: 40,
+        }),
+    ];
+
+    let vehicle_refs: Vec<&dyn Vehicle> = vehicles.iter().map(|v| v.as_ref()).collect();
+    println!("{:?}", all_models(vehicle_refs));
+}
--- delete_prefix/src/lib.rs
+++ delete_prefix/src/lib.rs
@@ -10,6 +10,6 @@ pub fn delete_prefix<'a>(prefix: &'a str, s: &'a str) -> Option<&'a str> {
 // use delete_prefix::delete_prefix;
 
 fn main() {
-	println!("{:?}", delete_prefix("ab", "abcdefghijklmnop"));
+	println!("{:?}", delete_prefix("abc", "abcdefghijklmnop"));
 	println!("{:?}", delete_prefix("x", "abcdefghijklmnop"));
 }
