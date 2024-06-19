ef9fa981e89fde2695dea9f8948e3b69ad9913c9
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-19 18:27:14 +0000

    closures 1.1

--- closures/src/lib.rs
+++ closures/src/lib.rs
@@ -1,6 +1,6 @@
 // Using closures and iterators create a function, that returns the first 50 even numbers squared in a Vec<i32>.
-fn first_fifty_even_square() -> Vec<i32> {
-    (0..100)
+pub fn first_fifty_even_square() -> Vec<i32> {
+    (1..=100)
        .filter(|x| x % 2 == 0)
        .map(|x| x * x)
        .collect()
