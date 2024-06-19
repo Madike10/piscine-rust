93d7f24be273646e1cae7ae7982431b77902d58d
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-19 01:47:35 +0000

    delete_prefix

---
+++ delete_prefix/src/lib.rs
@@ -0,0 +1,15 @@
+pub fn delete_prefix<'a>(prefix: &'a str, s: &'a str) -> Option<&'a str> {
+    if s.starts_with(prefix) {
+        Some(&s[prefix.len()..])
+    } else {
+        None
+    }
+}
+
+
+// use delete_prefix::delete_prefix;
+
+fn main() {
+	println!("{:?}", delete_prefix("ab", "abcdefghijklmnop"));
+	println!("{:?}", delete_prefix("x", "abcdefghijklmnop"));
+}
