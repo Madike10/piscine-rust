8b6f17692c8f54d6c8e9e2064220993969c2321a
ymadike <m.yade@isepdiamniadio.edu.sn>
2024-06-19 00:01:30 +0000

    lifetimes

---
+++ lifetimes/src/lib.rs
@@ -0,0 +1,21 @@
+#[derive(Debug)]
+pub struct Person<'a>{
+	pub name: &'a str,
+	pub age: u8,
+}
+
+impl Person<'_> {
+	pub fn new(name: &str) -> Person {
+        Person {
+            name: name,
+            age: 0,
+        }
+	}
+}
+
+
+fn main() {
+	let person = Person::new("Leo");
+
+	println!("Person = {:?}", person);
+}
